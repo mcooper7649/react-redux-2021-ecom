63e46f0b743a5678c7952fc7e492be65cd65ffad
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2021-12-28 20:55:16 -0500

    added header component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 
+import { Header } from './components/header/header.component';
+
 // const TopicsList = (props) => {
 //   console.log('TopicsList');
 //   return (
@@ -26,6 +28,7 @@ import ShopPage from './pages/shop/shop.component';
 function App() {
   return (
     <div>
+      <Header />
       <Router>
         <Route exact path="/">
           <HomePage />
