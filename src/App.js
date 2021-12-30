aa38cf23895ec9d3684d65511929908a23f8f5b4
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2021-12-29 22:59:58 -0500

    firebase auth google config and user object

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
@@ -7,24 +7,48 @@ import ShopPage from './pages/shop/shop.component';
 import { SignInSignUpPage } from './pages/sign-in-sign-up/sign-in-sign-up.component';
 
 import { Header } from './components/header/header.component';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Router>
-        <Route exact path="/">
-          <HomePage />
-        </Route>
-        <Route path="/shop">
-          <ShopPage />
-        </Route>
-        <Route path="/signin">
-          <SignInSignUpPage />
-        </Route>
-      </Router>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: null,
+    };
+  }
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({
+        currentUser: user,
+      });
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Router>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route path="/shop">
+            <ShopPage />
+          </Route>
+          <Route path="/signin">
+            <SignInSignUpPage />
+          </Route>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
