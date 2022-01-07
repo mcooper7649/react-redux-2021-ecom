5b08391951ef927a4ab43f9419e6c1cdc9f6455d
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-01-06 23:10:42 -0500

    setup redux part1 and create user reducer and root reducer

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -3,6 +3,7 @@ import './custom-button.styles.scss';
 const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => {
   return (
     <button
+      type="button"
       className={`${isGoogleSignIn ? 'google-sign-in ' : ''}custom-button`}
       {...otherProps}
     >
