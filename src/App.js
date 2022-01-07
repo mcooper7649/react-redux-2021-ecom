de2d702461ec70133732caed9b7578637854751b
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-01-06 22:06:06 -0500

    fixed react-router issue and auth bug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Switch, Route } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
@@ -14,7 +14,7 @@ class App extends React.Component {
     super();
 
     this.state = {
-      currentUser: null,
+      currentUser: {},
     };
   }
 
@@ -24,17 +24,18 @@ class App extends React.Component {
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
+        if (userRef) {
+          userRef.onSnapshot((snapShot) => {
+            this.setState({
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data(),
+              },
+            });
 
-        userRef.onSnapshot((snapShot) => {
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data(),
-            },
+            console.log(this.state);
           });
-
-          console.log(this.state);
-        });
+        }
       }
 
       this.setState({ currentUser: userAuth });
@@ -48,12 +49,14 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={SignInSignUpPage} />
-        </Switch>
+        <BrowserRouter>
+          <Header currentUser={this.state.currentUser} />
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/shop" component={ShopPage} />
+            <Route path="/signin" component={SignInSignUpPage} />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
--- src/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
+++ src/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import SignIn from '../../components/sign-in/sign-in.component';
 import SignUp from '../../components/sign-up/sign-up-component';
 
+import './sign-in-sign-up.styles.scss';
+
 export const SignInSignUpPage = () => {
   return (
-    <div className="sign-in-sign-up">
+    <div className="sign-in-and-sign-up">
       <SignIn />
       <SignUp />
     </div>
